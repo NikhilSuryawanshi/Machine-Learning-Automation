8da3ef50c6e52f390d5e7c03c8a2e61ed24653cb
Nikhil Suryawanshi <nikhilsurya29@gmail.com>
2020-05-25 16:38:01 +0530

    Add files via upload

---
+++ tweek.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+file= open("conf.txt", "r")
+count=int(file.readline())
+neuron=int(file.readline())
+epoch=int(file.readline())
+file.close()
+
+
+# In[2]:
+
+
+count+=1
+
+
+# In[3]:
+
+
+neuron*=2
+
+
+# In[4]:
+
+
+epoch*=2
+
+
+# In[5]:
+
+
+file= open("conf.txt", "w")
+file.write(str(count)+"\n"+str(neuron)+"\n"+str(epoch))
+file.close()
+
+
+# In[ ]:
+
+
+
+
